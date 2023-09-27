6cd151d4278d491a99c14b4be660a9c9f5d7512a
CreatorOfCryptids <dannypeck5000@gmail.com>
2023-09-27 15:53:56 -0400

    I forgot to save VariableReferenceNode.java before commiting, whoops.

--- VariableReferenceNode.java
+++ VariableReferenceNode.java
@@ -5,6 +5,16 @@ public class VariableReferenceNode extends Node{
     String name;
     Optional<Node> indexExpression;
 
+    VariableReferenceNode(String name){
+        this.name = name;
+        this.indexExpression = Optional.empty();
+    }
+
+    VariableReferenceNode(String name, Node indexExpression){
+        this.name = name;
+        this.indexExpression = Optional.of(indexExpression);
+    }
+
     public String toString() {
         // TODO
         String retVal = "";
